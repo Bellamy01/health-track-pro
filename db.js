fdac250bf9f33d33e901749aa5977a57ab15e0ac
Bellamy <bdanbellamy@gmail.com>
2023-10-15 19:08:53 +0200

    Created user model

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const db = new sqlite3.Database('/health.db');
 db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS patients (
-            id INTEGER PRIMARY KEY,
+            id STRING PRIMARY KEY,
             heartbeat INTEGER,
             body_temp REAL,
             patient_name TEXT,
@@ -12,6 +12,17 @@ db.serialize(() => {
             recent_sickness TEXT
         )
     `);
+
+    db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id STRING PRIMARY KEY,
+      name TEXT,
+      email TEXT UNIQUE,
+      password TEXT,
+      nationalID TEXT UNIQUE,
+      role TEXT
+    )
+  `);
 });
 
 module.exports = db;
---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const db = require('../db');
+
+exports.getAll = (call) => {
+    db.all('SELECT * FROM users', call);
+}
+
+exports.create = (user, callback) => {
+    db.run('INSERT INTO users (id, name, email, password, nationalID, role) VALUES (?, ?, ?, ?, ?)',
+    [user.id, user.name, user.email, user.password, user.nationalID, user.role], callback);    
+}
+
+exports.getOne = (id, callback) => {
+    db.get('SELECT * FROM users WHERE id = ?', [id], callback);
+}
+
+exports.update = (id, user, callback) => {
+    db.run('UPDATE users SET name = ?, email = ?, password = ?, nationalID = ? WHERE id = ?',
+    [user.name, user.email, user.password, user.nationalID, id], callback);
+}
+
+exports.updateRole = (id, user, callback) => {
+    db.run(`UPDATE users SET role = ? WHERE id = ?`,
+    [user.role, id], callback);
+}
+
+exports.delete = (id, callback) => {
+    db.run('DELETE FROM users WHERE id = ?', [id], callback);
+}
